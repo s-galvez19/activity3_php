19ee5ab2bccafe7dd6d6cdeb0d9b19c83ccc31aa
unknown <sgalvez@techlaunch.io>
2019-05-06 17:44:07 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<?php
+$profile = [
+	"name" => "Teresa Santos",
+	"age" => "19",
+	"position" => "Junior Developer",
+	"gender" => "F",
+	"facebook_link" => "https://www.facebook.com/teresa",
+	"github_link" => "https://github.com/teresa",
+	"bio" => "Teresa is an young entrepreneur living in Miami who likes ice cream and watches superheroes movies at night. She is a happy fellow.",
+	"picture" => "https://www.imagen.com.mx/assets/img/imagen_share.png"
+];
+
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link  rel="stylesheet" href="style.css" >
+    <title>Document</title>
+</head>
+<body>
+    <!-- Add icon library -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+
+<div class="card">
+  <img src="scarlet.jpg" alt="John" style="width:100%">
+  <h1>Scarlett johansson</h1>
+  <p class="title">Actress</p>
+  <p class="title">34 years old</p>
+  <p class="title">Scarlett is an young entrepreneur living in california
+  who likes ice cream and watches superheros movies like avengers. She is a happy below.</p>
+  <p>Harvard University</p>
+  <a href="#"><i class="fa fa-dribbble"></i></a> 
+  <a href="#"><i class="fa fa-twitter"></i></a> 
+  <a href="#"><i class="fa fa-linkedin"></i></a> 
+  <a href="#"><i class="fa fa-facebook"></i></a> 
+  <p><button>Contact</button></p>
+</div>
+</body>
+</html>
+
